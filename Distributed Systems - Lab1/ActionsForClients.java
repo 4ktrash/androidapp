98e7d40de678632fda765eb16b4885fc073567fb
ElMuchoGreco <39050247+ElMuchoGreco@users.noreply.github.com>
2020-03-10 13:02:04 +0200

    Add files via upload

---
+++ Distributed Systems - Lab1/ActionsForClients.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class ActionsForClients extends Thread {
+    ObjectInputStream in;
+    ObjectOutputStream out;
+
+    public ActionsForClients(Socket connection) {
+        try {
+            out = new ObjectOutputStream(connection.getOutputStream());
+            in = new ObjectInputStream(connection.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            /*
+             *
+             *
+             *
+             */
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+}
